3283f71580f6734692a4f09003e30e8062f9e8de
Greg Bree <164895001+goodalgebra@users.noreply.github.com>
2024-03-25 16:15:19 +0200

    Create maincode.cpp

---
+++ maincode.cpp
@@ -0,0 +1,20 @@
+ const string::const_iterator& it_end, int64_t& value)
+{
+  value = 0;
+  while (s_it != it_end && *s_it == ' ') ++s_it;
+  if (s_it == it_end) return false;
+  bool is_neg = (*s_it == '-');
+  if (is_neg) ++s_it;
+  string::const_iterator s_it2 = s_it;
+  while (s_it != it_end && *s_it >= '0' && *s_it <= '9')
+  {
+    value = value * 10 + static_cast<int64_t>(*s_it++) - 48;
+  }
+
+  if (s_it == s_it2) return false; //no value
+  //trim trailing space and a comma if present
+  while (s_it != it_end && *s_it == ' ') ++s_it;
+  if (s_it != it_end && *s_it == ',') ++s_it;
+  if (is_neg) value = -value;
+  return true;
+}
